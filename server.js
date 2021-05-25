d29366f7d9b8a263bdc7fb5d79b94b62f6e785b0
Pranjal Shikhar Sinha <shikhar.pranjal3@gmail.com>
2021-05-25 15:05:13 +0530

    🐱‍👤 Updated server.js

--- server.js
+++ server.js
@@ -1,6 +1,17 @@
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
+const ejs = require('ejs');
+const expressLayout = require('express-ejs-layouts');
+const path = require('path');
+
+
+
+// Set Template
+app.use(expressLayout);
+app.set('views', path.join(__dirname, './resources/views'));
+app.set('view engine', 'ejs');
+
 
 
 app.listen(PORT, ()=> {
