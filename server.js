eaa7a0ca54c8180c10a433d00032ad54d04191d0
Pranjal Shikhar Sinha <shikhar.pranjal3@gmail.com>
2021-05-27 20:00:45 +0530

    🍪 Added Session and Cookies

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -5,6 +6,12 @@ const ejs = require('ejs');
 const expressLayout = require('express-ejs-layouts');
 const path = require('path');
 const mongoose = require('mongoose');
+const session = require('express-session');
+const flash = require('express-flash');
+const MongoDbStore = require('connect-mongo');
+
+
+app.use(flash());
 
 
 // Assets
@@ -20,6 +27,19 @@ connection.once('open', () => {
 }).catch(err => {
     console.log('Connection Failed..');
 });
+
+
+
+// Session Config
+app.use(session({
+    secret: process.env.COOKIE_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    cookie: { maxAge: 1000 * 60 * 60 * 24 },
+    store: MongoDbStore.create({
+        mongoUrl: url
+    }),
+}));
 
 
 
