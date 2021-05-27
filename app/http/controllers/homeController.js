ea6cfabc44b164ae9fe4760c0de6d649d45b2992
Pranjal Shikhar Sinha <shikhar.pranjal3@gmail.com>
2021-05-27 19:05:09 +0530

    🍃 Connected to MongoDB

--- app/http/controllers/homeController.js
+++ app/http/controllers/homeController.js
@@ -1,7 +1,11 @@
+const Menu = require('../../models/menu');
+
 const homeController = () => {
     return {
-        index(req, res) {
-            res.render('home');
+        async index(req, res) {
+            const pizzas = await Menu.find()
+            console.log(pizzas);
+            return res.render('home', { pizzas: pizzas });
         }
     }
 }
--- server.js
+++ server.js
@@ -4,9 +4,22 @@ const PORT = process.env.PORT || 3000;
 const ejs = require('ejs');
 const expressLayout = require('express-ejs-layouts');
 const path = require('path');
+const mongoose = require('mongoose');
 
-// Use CSS
+
+// Assets
 app.use(express.static(__dirname +  '/public'));
+
+
+// Database Connection
+const url = 'mongodb://localhost/pizza';
+mongoose.connect(url, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true, useFindAndModify: true });
+const connection = mongoose.connection;
+connection.once('open', () => {
+    console.log('Database Connected..');
+}).catch(err => {
+    console.log('Connection Failed..');
+});
 
 
 
@@ -15,8 +28,10 @@ app.use(expressLayout);
 app.set('views', path.join(__dirname, '/resources/views/'));
 app.set('view engine', 'ejs');
 
+
 // Call Routes
 require('./routes/web')(app);
+
 
 app.listen(PORT, ()=> {
     console.log(`Connected at ${PORT}`);
