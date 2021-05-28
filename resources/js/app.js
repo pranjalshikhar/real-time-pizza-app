7a0dcf4a0ff3b8eeb63171bda130956d9c107b5b
Pranjal Shikhar Sinha <shikhar.pranjal3@gmail.com>
2021-05-28 16:01:26 +0530

    📢 Noty Added

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,12 +1,25 @@
 import axios from 'axios'
+import Noty from 'noty'
 
 let addToCart = document.querySelectorAll('.add-to-cart')
 let cartCounter = document.querySelector('#cartCounter')
 
 const updateCart = (pizza) => {
     axios.post('/update-cart', pizza).then(res => {
-        console.log(res)
         cartCounter.innerText = res.data.totalQty
+        new Noty({
+            type: 'success',
+            timeout: 1000,
+            progressBar: false,
+            text: 'Item added to cart.'
+        }).show();
+    }).catch(err => {
+        new Noty({
+            type: 'error',
+            timeout: 1000,
+            progressBar: false,
+            text: 'Something went wrong'
+        }).show();
     })
 }
 
